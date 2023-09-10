4fe514634bc55e115a54c8054871e117936ded61
Carles Pey <carles.pey@gmail.com>
2023-09-09 20:09:54 -0400

    Search elastic objects

--- README.md
+++ README.md
@@ -12,7 +12,6 @@ $ ./objscan.py -s 1024
 Result in file objscan_kmalloc_1024.txt
 $ head objscan_kmalloc_1024.txt
 paravirt_patch_template
-xregs_state
 user_namespace
 hrtimer_cpu_base
 css_set
@@ -20,5 +19,18 @@ device
 inode
 cgroup_bpf
 psi_group
-intel_excl_cntrs
+x86_pmu
+bdi_writeback
+~~~
+
+Option *-e* proposes suitable elastic objects together with the ones falling in
+the specified slab size. See the output below, where the elastic object is
+flagged with *[e]*:
+
+~~~
+[cpey@nuc objscan]$ ./objscan.py -e -s 96 -o
+file_system_type
+tracepoint
+...
+dev_ifalias [e]
 ~~~
--- objscan.py
+++ objscan.py
@@ -4,22 +4,51 @@ This program scans the output of pahole(1) for suitable kernel objects to
 use in UAF vulnerability exploitation.
 """
 import argparse
-import json
 import os.path
 import random
 import re
 import string
 import subprocess
+import sys
 from datetime import datetime
 
 
 SLABS = [8, 16, 32, 64, 96, 128, 192, 256, 512, 1024, 2048, 4096, 8192]
 SUFFIX_LEN = 10
+IS_FINE_REGEX = [
+    # function pointers
+    ".*\(\*.*\)",
+    # list_head structs
+    "struct +list_head",
+    # pointers to structs with `op` in their names
+    "struct +.*(ops|operations) +\*",
+]
+IS_ELASTIC_REGEX = "char +.*\[\]"
+COMMENT_LINE_REGEX = "\t/\*((?!/\*).)* \*/$"
+CLOSING_LINE_REGEX = "};"
+EMPTY_LINE_REGEX = "$"
 
 def get_tmp_filename():
     random.seed(datetime.now().timestamp())
     suffix = ''.join(random.choice(string.ascii_letters) for i in range(SUFFIX_LEN))
-    return "/tmp/objscan_{}".format(suffix)
+    return "/tmp/objscan-{}".format(suffix)
+
+def get_output_filename(idx, in_file, elastic):
+    if elastic:
+        out = f"objscan_elastic_kmalloc_{SLABS[idx]}"
+    else:
+        out = f"objscan_kmalloc_{SLABS[idx]}"
+    if in_file:
+        out = f"{out}_for_{in_file}.txt"
+    else:
+        out = f"{out}.txt"
+    return out
+
+def store_result(fdw, result):
+    if fdw:
+        fdw.write(result)
+    else:
+        print(result, end="")
 
 def find_slab_idx(size):
     i = 0
@@ -37,69 +66,104 @@ def get_all_objects(fname):
     subprocess.run(["pahole", "--sizes"], stdout=fd)
     fd.close()
 
-def looks_good(obj):
-    proc = subprocess.Popen(["pahole", "-E", obj], stdout=subprocess.PIPE)
-    count = 0
+def check_member_is_fine(line):
+    i = 0
     good = False
+    while i<len(IS_FINE_REGEX) and not good:
+        if re.search(IS_FINE_REGEX[i], line):
+            good = True
+            break
+        i += 1
+    return good
+
+def looks_good(obj, elastic):
+    proc = subprocess.Popen(["pahole", "-E", obj], stdout=subprocess.PIPE)
+    comment_line = re.compile(COMMENT_LINE_REGEX)
+    closing_line = re.compile(CLOSING_LINE_REGEX) 
+    empty_line = re.compile(EMPTY_LINE_REGEX) 
+    good = False
+    last_member = None
+    member = ''
     while True:
-        count += 1
-        # Skip the name of the struct
-        if count == 1:
-            continue
         line = proc.stdout.readline()
         if not line:
             break
         line = str(line, "UTF-8")
-        # function pointers
-        if re.search(".*\(.*\)", line):
-            good = True
+        if comment_line.match(line):
+            continue
+        if closing_line.match(line):
+            continue
+        if empty_line.match(line):
+            continue
+        member = line
+        if not good:
+            good = check_member_is_fine(member)
+        if not elastic and good:
             break
-        # list_head structs
-        if re.search("struct +list_head", line):
-            good = True
-            break
-        # pointers to structs with `op` in their names
-        if re.search("struct +.*(ops|operations) +\*", line):
-            good = True
-            break
+
+    if good and elastic and not re.search(IS_ELASTIC_REGEX, member):
+        good = False
     return good
 
-def get_obj_for_slab(out, tmp, target):
+def get_obj_for_slab(target, out, tmp, elastic):
     prv_size = SLABS[target-1]
     tgt_size = SLABS[target]
     fdr = open(tmp, "r", encoding="utf-8")
-    fdw = open(out, "w", encoding="utf-8")
+    fdw = None
+    if out:
+        fdw = open(out, "w", encoding="utf-8")
+
     while True:
         line = fdr.readline()
         if not line:
             break
         obj, size = re.findall("([_A-Za-z0-9]+)\t", line)
         size = int(size)
-        if size <= prv_size or size > tgt_size:
-            continue
-        if looks_good(obj):
-            fdw.write(f"{obj}\n")
+        if prv_size < size <= tgt_size:
+            if looks_good(obj, False):
+                store_result(fdw, f"{obj}\n")
+        elif size <= prv_size and elastic:
+            if looks_good(obj, True):
+                store_result(fdw, f"{obj} [e]\n")
 
     fdr.close()
-    fdw.close()
+    if out:
+        fdw.close()
 
-def get_obj_by_size(size):
+def get_obj_by_size(size, in_file, stdout, elastic):
     found, idx = find_slab_idx(int(size))
     if not found:
-        print("No slab available for given size: {} bytes".format(size))
-        exit(-1)
-    tmp = get_tmp_filename()
-    get_all_objects(tmp)
-    out = "objscan_kmalloc_{}.txt".format(SLABS[idx])
-    get_obj_for_slab(out, tmp, idx)
-    os.remove(tmp)
-    print("Result in file {}".format(out))
+        print(f"No slab available for given size: {size} bytes")
+        sys.exit(-1)
+    out = None
+    if not stdout:
+        out = get_output_filename(idx, in_file, elastic)
+    tmp = in_file
+    if not in_file:
+        tmp = get_tmp_filename()
+        get_all_objects(tmp)
+    get_obj_for_slab(idx, out, tmp, elastic)
+    if not in_file:
+        os.remove(tmp)
+    if out:
+        print(f"Result in file {out}")
+
+def get_elastic_objs():
+    pass
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
                     prog='objscan',
                     description='Looks for a suitable kernel object')
-    parser.add_argument('-s', '--size', required=True,
-                        help='Size of the object to find')
+    parser.add_argument('-i', '--input', required=False,
+                        help='Use the specified pahole output')
+    parser.add_argument('-s', '--size', required=False,
+                        help='Scan for objects of the the specified size')
+    parser.add_argument('-e', '--elastic', required=False, action='store_true',
+                        default=False, help='Scan for elastic objects')
+    parser.add_argument('-o', "--stdout", required=False, action='store_true',
+                        default=False, help='Write resul to the standard'
+                        ' output')
     args = parser.parse_args()
-    get_obj_by_size(args.size)
+    if args.size:
+        get_obj_by_size(args.size, args.input, args.stdout, args.elastic)
